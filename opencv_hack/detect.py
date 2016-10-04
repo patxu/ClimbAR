bf42e4c10048829f876dd3e6f1cc1b9b9aebb0ac
patxu <patrick.s.xu.17@dartmouth.edu>
2016-10-04 01:33:11 -0400

    success! sortof

--- opencv_hack/detect.py
+++ opencv_hack/detect.py
@@ -3,7 +3,7 @@ import cv2
 
 hold_cascade = cv2.CascadeClassifier('train_cascade/cascade.xml')
 
-img = cv2.imread('img/hold_view2.jpg')
+img = cv2.imread('img/test/63out.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 holds = hold_cascade.detectMultiScale(gray, 1.3, 5)
