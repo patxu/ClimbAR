15fe2c05420748d0822cb37cdff6e0bf8fac7c5f
Patrick Xu <patrick.s.xu.17@dartmouth.edu>
2017-02-01 20:48:28 -0500

    Added better error handling in dll
    Now dispose frame on error and yield break rather than null. Also added
    cleanupBBArray function to clean up memory allocated for hold array

--- OpenCVUnity/OpenCVUnity/opencvtest2.h
+++ OpenCVUnity/OpenCVUnity/opencvtest2.h
@@ -4,5 +4,6 @@
 
 extern "C" {
 	DllExport int* classifyImage(const char* classifierPath, unsigned char* data, int height, int width);
+	DllExport void cleanupBBArray();
 	DllExport int* findProjectorBox(unsigned char* redData, unsigned char* greenData, unsigned char* blueData, int imageWidth, int imageHeight);
 }
