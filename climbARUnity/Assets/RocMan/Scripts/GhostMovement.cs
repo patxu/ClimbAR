85eb1967f13978af0fa0b4e5bed192cc61ca0d38
Patrick Xu <patrick.s.xu.17@dartmouth.edu>
2017-02-05 18:50:36 -0500

    Migrate last old changes to Rocman Game

--- climbARUnity/Assets/RocMan/Scripts/GhostMovement.cs
+++ climbARUnity/Assets/RocMan/Scripts/GhostMovement.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GhostMovement : MonoBehaviour {
+public class GhostMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // State variables
+    private float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.moveSpeed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, this.moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -this.moveSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene("YouDied");
+    }
 }
