0be05ca493b58e7fa120c245fce00a059dbb9a78
patxu <patrick.s.xu.17@dartmouth.edu>
2017-03-03 17:38:03 -0500

    progress on smoothing

--- climbARUnity/Assets/Climbing Hold/ClimbingHold.cs
+++ climbARUnity/Assets/Climbing Hold/ClimbingHold.cs
@@ -1,21 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Threading;
 
 
 public class ClimbingHold : MonoBehaviour
 {
     private int enterCount;
-    private System.DateTime lastCountedCollision;
-    private int smoothing = 750;
+    private bool endStateKnown;
+    private float grabbedSmoothing = 500 / 1000; // divided by 1000 to put as fraction of second
+    private float releasedSmoothing = 500 / 1000;
+
     public bool smoothingEnabled;
+    public enum States {Released, GrabbedPending, Grabbed, ReleasePending}
+    public States currentState;
+
+    private Guid id;
 
 
     void Start()
     {
         enterCount = 0;
         smoothingEnabled = true;
-        lastCountedCollision = System.DateTime.UtcNow;
+        currentState = States.Released;
+        endStateKnown = true;
+        id = Guid.NewGuid();
     }
 
     void Update()
@@ -23,55 +32,134 @@ public class ClimbingHold : MonoBehaviour
         
     }
 
+    private IEnumerator grabbedPending()
+    {
+        endStateKnown = false;
+        Debug.Log("About to sleep in state " + currentState.ToString());
+        yield return new WaitForSeconds(0.1f);
+
+        Debug.Log("Just woke up in state " + currentState.ToString() + " with enterCount: " + enterCount.ToString());
+        // if after the sleep we are still in pending state, change to grabbed
+        if (currentState == States.GrabbedPending)
+        {
+            currentState = States.Grabbed;
+            endStateKnown = true;
+        }
+    }
+
+    private IEnumerator releasedPending()
+    {
+        endStateKnown = false;
+
+        yield return new WaitForSeconds(0.1f);
+
+        // if after the sleep we are still in pending state, change to released
+        if (currentState == States.ReleasePending) ;
+        {
+            currentState = States.Released;
+            endStateKnown = true;
+        }
+    }
+
+    private void updateStateOnCollisionExit()
+    {
+        Debug.Log("hold " + id + " updated exit with state " + currentState.ToString() + " and enterCount " + enterCount.ToString());
+        switch (currentState)
+        {
+            case States.Released:
+                break;
+            case States.GrabbedPending:
+                if (enterCount == 0)
+                {
+                    Debug.Log("Whoops! hold " + id + " when grabbed pending comes back we will be released");
+                    currentState = States.Released;
+                }
+                break;
+            case States.Grabbed:
+                if (enterCount == 0)
+                {
+                    currentState = States.ReleasePending;
+                    StartCoroutine("releasedPending");
+                }
+                break;
+            case States.ReleasePending:
+                break;
+        }
+    }
+
+    private void updateStateOnCollisionEnter()
+    {
+        Debug.Log("hold " + id + " updated enter with state " + currentState.ToString() + " and enterCount " + enterCount.ToString());
+        switch (currentState)
+        {
+            case States.Released:
+                currentState = States.GrabbedPending;
+                Debug.Log("starting coroutine");
+                StartCoroutine("grabbedPending");
+                break;
+            case States.GrabbedPending:
+                break;
+            case States.Grabbed:
+                break;
+            case States.ReleasePending:
+                currentState = States.Grabbed;
+                break;
+        }
+    }
+
     public bool ShouldRegisterHoldGrabbed(Collider2D col)
     {
+        // Check that not colliding with overlapping hold
         if (col != null && col.gameObject.tag == "Hold")
         {
             return false;
         }
-        else if (smoothingEnabled)
+        
+        // If we don't want smoothing, just return true
+        if (!smoothingEnabled)
         {
-            enterCount++;
-            System.DateTime currentTime = System.DateTime.UtcNow;
-            TimeSpan diff = currentTime - lastCountedCollision;
-
-            if (enterCount > 1 || diff.TotalMilliseconds < smoothing)
-            {
-                return false;
-            }
-
-            lastCountedCollision = System.DateTime.UtcNow;
             return true;
+        }
+        enterCount++;
+
+        updateStateOnCollisionEnter();
+        Debug.Log("Returning with state: " + currentState.ToString());
+
+        if (endStateKnown)
+        {
+            return currentState == States.Grabbed;
         }
         else
         {
-            return true;
+            Debug.Log("State not know");
+            return false;
         }
     }
 
     public bool ShouldRegisterHoldReleased(Collider2D col)
     {
+        // Check that not colliding with overlapping hold
         if (col != null && col.gameObject.tag == "Hold")
         {
             return false;
         }
-        else if (smoothingEnabled)
-        {
-            enterCount--;
 
-            if (enterCount == 0)
-            {
-                lastCountedCollision = System.DateTime.UtcNow;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else 
+        // If we don't want smoothing, just return true
+        if (!smoothingEnabled)
         {
             return true;
+        }
+        enterCount--;
+        updateStateOnCollisionExit();
+
+        if (endStateKnown)
+        {
+            return currentState == States.Released;
+        }
+        else
+        {
+            Debug.Log("State not known");
+            return false;
         }
     }
 
