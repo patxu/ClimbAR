34454260025b270e28d52494cf112c10b1f9e3f9
patxu <patrick.s.xu.17@dartmouth.edu>
2017-03-05 13:36:48 -0500

    Added quit confirmation to non-game scenes
    To add quit confirmation to a scene, simply copy ConfirmCanvas
    and paste on Main Camera for the scene.

--- climbARUnity/Assets/Shared/Scripts/ExitManager.cs
+++ climbARUnity/Assets/Shared/Scripts/ExitManager.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ExitManager : MonoBehaviour {
-    public Button confirmButton;
-    public Button cancelButton;
 	// Use this for initialization
 	void Start () {
-        confirmButton.onClick.AddListener(ExitConfirmed);
-        cancelButton.onClick.AddListener(ExitCancled);
 	}
 	
 	// Update is called once per frame
@@ -30,10 +26,13 @@ public class ExitManager : MonoBehaviour {
     }
 
     public void ExitCancled()
-    { 
+    {
         GameObject bodyView = GameObject.Find("KinectBodyView");
-        BodySourceView view = bodyView.GetComponent<BodySourceView>();
-        view.shouldShowTextMesh = true;
+        if (bodyView != null)
+        {
+            BodySourceView view = bodyView.GetComponent<BodySourceView>();
+            view.shouldShowTextMesh = true;
+        }
 
         GameObject confirmCanvas = GameObject.Find("ConfirmCanvas");
         CanvasGroup exitGroup = confirmCanvas.GetComponent<CanvasGroup>();
