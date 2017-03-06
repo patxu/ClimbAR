0da7348d13604714b59877c69b272b5cf3cd4d7f
olykos <orestis.d.lykouropoulos.17@dartmouth.edu>
2017-03-05 21:02:34 -0500

    Add exit confirmation for RocMan and work on option to change game state in RocMan

--- climbARUnity/Assets/RocMan/Scripts/GhostMovement.cs
+++ climbARUnity/Assets/RocMan/Scripts/GhostMovement.cs
@@ -8,39 +8,46 @@ public class GhostMovement : MonoBehaviour
 
     // State variables
     private float moveSpeed;
-    private int lives;
+    public RocMan rocmanScript;
 
     // Use this for initialization
     void Start()
     {
         this.moveSpeed = 0.5f;
-        this.lives = 10;
+        this.rocmanScript = Camera.main.GetComponent<RocMan>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (this.rocmanScript.playing)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, this.moveSpeed);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, this.moveSpeed);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -this.moveSpeed);
+            }
+            // For Debug
+            if (Input.GetKey("space"))
+            {
+                this.rocmanScript.ToggleEndGame();
+            }
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            // Listen for 'm' or 'r'
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-this.moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -this.moveSpeed);
-        }
-    }
-
-    void ReverseDirection()
-    {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * (-1), GetComponent<Rigidbody2D>().velocity.y * (-1));
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -50,17 +57,17 @@ public class GhostMovement : MonoBehaviour
         switch (layerName)
         {
             case "Holds":
-                // currently a nop
                 break;
             case "Skeleton":
-                this.lives--;
+                this.rocmanScript.LoseLife();
                 break;
             default:
                 break;
         }
-        if (this.lives <= 0)
-        {
-           SceneManager.LoadScene(SceneUtils.SceneNames.rocManYouDied);
-        }
+    }
+
+    public void ReverseDirection()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x * (-1), GetComponent<Rigidbody2D>().velocity.y * (-1));
     }
 }
